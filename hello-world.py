be3b70a3c2b50157aee85cfda64f83211ba4a4d6
Hafsat Abbas <=>
2025-09-10 13:56:21 +0100

    i leant concatenation and how to join two or more word together

--- hello-world.py
+++ hello-world.py
@@ -18,3 +18,36 @@ a2 = 6
 a3 = 2
 
 print(a1 + a2 + a3)
+
+
+firstName = "Hafsat"
+lastName = "Abbas"
+
+fullName = firstName + " " + lastName + "."
+print(fullName)
+
+product = "iphone"
+model = "xr"
+productFullname = product + " " + model
+print(productFullname)
+
+x1 = "h"
+x2 = "e"
+x3 = "l"
+x4 = "l"
+x5 = "o"
+word = x1 + x2 + x3 + x4 + x5
+print(word)
+
+y1 = "w"
+y2 = "o"
+y3 = "r"
+y4 = "ld"
+word1 = y1 + y2 + y3 + y4
+print(word1)
+print(word + " " + word1) 
+
+h2 = "m" 
+h3 = "e"
+word3 = h2 + " " + h3 + "."
+print(word3)
